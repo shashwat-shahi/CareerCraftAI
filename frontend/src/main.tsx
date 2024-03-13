2b8d8835ef2062358adc323815697648c220f7f9
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-03-13 16:28:01 -0400

    Add icon and title

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Hello from './components/test/Hello.tsx'
-import Bye from './components/test/Bye.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "@/components/theme-provider"
 import UploadResume from './pages/UploadResume.tsx'
@@ -14,8 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} /> 
-          <Route path="hello" element={<Hello/>} />
-          <Route path="bye" element={<Bye />} />
           <Route path="upload" element={<UploadResume />} />  
         </Routes>
       </BrowserRouter>
