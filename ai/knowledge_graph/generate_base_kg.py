f3a272102ff2e7f3a6d0666f36d67bc7caed36de
Shashwat Shahi <shahi.sh@northeastern.edu>
2024-03-14 17:45:06 -0400

    Add Read JSON Mech

--- ai/knowledge_graph/generate_base_kg.py
+++ ai/knowledge_graph/generate_base_kg.py
@@ -10,8 +10,15 @@ uri = "neo4j+s://aa36fcd8.databases.neo4j.io"
 username = "neo4j"
 password = "j29FcGopBPohDy3Ynkr69TYKcjXP5fLqcW5Ve-c7PO0"
 
+# Read and write JSON files
+def read_json_file(filename, data_dir):
+    with open(os.path.join(data_dir, filename), "r") as file:
+        data = json.load(file)
+    return data
 
 if __name__ == "__main__":
+    # Read JSON data
+    json_data = read_json_file("2_processed_android_developer_roadmap.json", "ai/knowledge_graph/data/second_staged_processed_data")
 
     # Connect to Neo4j AuraDB
     driver = GraphDatabase.driver(uri, auth=(username, password))
