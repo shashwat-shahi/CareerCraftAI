f5ef4c8da87053dc33af39e46a38424aa0e5d3de
Yesha Joshi <joshi.ye@northeastern.edu>
2024-03-14 19:28:55 -0400

    create login page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -8,39 +8,44 @@ import {
    
 function Auth() {
   return (
-    <div className="flex min-h-[90vh]">
-        <Card className="w-[400px] self-center mx-auto">
-            <CardHeader>
-                <CardTitle className='mb-3'>Log In to Jumpstart Your Career</CardTitle>
-                <CardDescription className='mb-5'>Securely log in now to start getting personalized resume feedback and discover exciting job opportunities tailored just for you</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Button // @ts-ignore 
-                            >
-                                <Avatar>
-                                    <AvatarImage src="github_logo.svg" alt="@shadcn" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                Github
-                            </Button>
+    <div className='wrapper flex'>
+        <div className='left w-[60%]'>
+            <img src="/assets/auth.jpg" alt="auth" />
+        </div>
+        <div className="right w-[40%] flex min-h-[90vh]">
+            <Card className="w-[400px] self-center mx-auto">
+                <CardHeader>
+                    <CardTitle className='mb-3'>Log In to Jumpstart Your Career</CardTitle>
+                    <CardDescription className='mb-5'>Securely log in now to start getting personalized resume feedback and discover exciting job opportunities tailored just for you</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Button 
+                                >
+                                    <Avatar>
+                                        <AvatarImage src="github_logo.svg" alt="@shadcn" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    Github
+                                </Button>
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Button 
+                                variant="outline">
+                                    <Avatar>
+                                        <AvatarImage src="google_logo.svg" alt="@shadcn" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    Google
+                                </Button>
+                            </div>
                         </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Button // @ts-ignore
-                            variant="outline">
-                                <Avatar>
-                                    <AvatarImage src="google_logo.svg" alt="@shadcn" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                Google
-                            </Button>
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-        </Card>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
     </div>
   )
 }
