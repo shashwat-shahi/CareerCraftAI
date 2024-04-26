97bfd07235a69226a082866c2b33bb6968f13ad5
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-04-26 00:17:18 -0400

    Setup config file in python

--- ai/app.py
+++ ai/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from resume_entity_extraction import extract_details_from_resume
 from flask import request, jsonify
+import config
+import logging
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.INFO)
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -12,8 +14,8 @@ def hello():
 def extractResumeDetails():
     # Get the filename and user id from the request data
     filename = request.form.get('filename')
-    print(f'filename: {filename}')
     user_id = request.form.get('user_id')
+    logging.info("API KEY " + config.API_KEY)
     user_details = extract_details_from_resume(filename)
     return jsonify(user_details)
 
