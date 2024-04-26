d64794a0d868742b7aee7889296fb110d5d2a590
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-04-26 16:48:13 -0400

    Change redux

--- frontend/src/state/user/userSlice.ts
+++ frontend/src/state/user/userSlice.ts
@@ -2,31 +2,29 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: {},
-}
+};
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(
-        getUserAsync.fulfilled,
-        (state, action) => {
-          state.value = action.payload;
-        }
-      )
+    builder.addCase(getUserAsync.fulfilled, (state, action) => {
+      state.value = action.payload.responseBody;
+    });
   },
-})
+});
 
 export const getUserAsync = createAsyncThunk(
-    "user/getUserAsync",
-    async (userId: string) => { 
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/getUser/${userId}`, {credentials: 'include'})
-        const data = await response.json()
-        return data;
-    }
-  )
-
+  "user/getUserAsync",
+  async (userId: string) => {
+    const response = await fetch(
+      `${import.meta.env.VITE_BACKEND_URL}/user/getUser/${userId}`,
+      { credentials: "include" }
+    );
+    const data = await response.json();
+    return data;
+  }
+);
 
 export default userSlice.reducer;
