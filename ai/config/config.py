c9f57ad54801152d448f4ef39bf95080813fa1a8
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-04-26 00:00:13 -0400

    Create config python file

---
+++ ai/config/config.py
@@ -0,0 +1,17 @@
+# Google Bard API key
+API_KEY = 'AIzaSyAA3BQfehEelyg9cz9PZZGKGrfQ1PlK2XA'
+
+# Neo4j database connection
+NEO4J_URI = 'neo4j+s://aa36fcd8.databases.neo4j.io'
+NEO4J_USERNAME = 'neo4j'
+NEO4J_PASSWORD = 'j29FcGopBPohDy3Ynkr69TYKcjXP5fLqcW5Ve-c7PO0'
+
+# Data directory
+DATA_DIRECTORY = 'ai/knowledge_graph/data/final_processed_data/'
+
+# AWS credentials
+AWS_ACCESS_KEY = 'AKIAW3MEEUNOKCJLMUW3'
+AWS_SECRET_KEY = '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'
+AWS_REGION = 'us-east-2'
+AWS_STACK_AUTO = False
+AWS_BUCKET_NAME = 'ccresumes'
