b4c4e38b75c4fdba24edf447fe04a72d7e59eee8
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-04-26 14:47:58 -0400

    Fix resume flow

--- ai/app.py
+++ ai/app.py
@@ -3,9 +3,15 @@ from resume_entity_extraction import extract_details_from_resume
 from flask import request, jsonify
 import config
 import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Configure CORS to allow access only from specific origins
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://careercraftai.sarveshsawant.com"]}})
+
 logging.basicConfig(level=logging.INFO)
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -15,8 +21,10 @@ def extractResumeDetails():
     # Get the filename and user id from the request data
     filename = request.form.get('filename')
     user_id = request.form.get('user_id')
-    logging.info("API KEY " + config.API_KEY)
+    logging.info("sarvesh filename " + filename)
+    logging.info("sarvesh userid " + user_id)
     user_details = extract_details_from_resume(filename)
+    
     return jsonify(user_details)
 
 if __name__ == "__main__":
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
-import resume_details from "../data/resume_details.json"
+
+// @ts-nocheck
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -7,20 +8,72 @@ import {
 import PersonalInfo from "./Dashboard/PersonalInfo";
 import Skills from "./Dashboard/Skills";
 import WorkExperience from "./Dashboard/WorkExperience";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/state/store";
+import { getUserAsync } from "@/state/user/userSlice";
+
 
 const Dashboard = () => {
-  const data = resume_details
-  console.log(data)
+  
+  const [fetchedUserData, setFetchedUserData] = useState(false)
+  let userId = localStorage.getItem("userId")
+
+  const userJson:any = useSelector((state: RootState) => state.user.value)
+
+  const dispatch = useDispatch<AppDispatch>();
+    useEffect(() => {
+    dispatch(getUserAsync(userId))
+    setFetchedUserData(true)
+  }, [userId])
+
+  const url = `${import.meta.env.VITE_BACKEND_URL}/user/getResume?fileName=${userJson?.resumeLink}`; 
+  
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const formData = new FormData();
+
+        console.log("inside useeffect " + userJson?.resumeLink + " userId "+userId)
+        formData.append("filename", userJson?.resumeLink);
+        formData.append("user_id", userId);
+
+        const result = await fetch(
+          `${import.meta.env.VITE_AI_URL}/extractResumeDetails`,
+          {
+            method: 'POST',
+            body: formData
+          }
+        );
+        const jsonResponse = await result.json();
+        setResponse(jsonResponse);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    if(userJson){
+      fetchData();
+    }
+      
+ 
+  }, [userJson]);
+
+  
+
   return (
     <div className="flex justify-center mt-5">
-      <Card className="w-[750px] p-8">
-        <PersonalInfo name={data.name} email={data.email} mobileNumber={data.mobile_number} />
-        <Skills skills={data.skills} />
-        <WorkExperience workDetails={data.work_details} />
+      {response && <Card className="w-[750px] p-8">
+        <PersonalInfo name={response?.name} email={response?.email} mobileNumber={response?.mobile_number} />
+        <Skills skills={response?.skills} />
+        <WorkExperience workDetails={response?.work_details} />
         <CardFooter className="flex justify-center mt-4">
           <Button className="w-full">View Suggested Courses</Button>
         </CardFooter>
-      </Card>
+      </Card>}
     </div>
   );
 };
