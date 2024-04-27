3bfe24683298a306adffecd5094d77a93092661b
Sarvesh Sawant <sawant.sar@northeastern.edu>
2024-04-27 10:33:04 -0400

    Minox fix on login

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,57 +1,38 @@
-import { useState } from 'react';
-import { Button } from "@/components/ui/button";
-import { useNavigate } from 'react-router-dom';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from "@/components/ui/button"
+import { Link } from 'react-router-dom'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function Auth() {
-  const navigate = useNavigate();
-  const [isLoggingIn, setIsLoggingIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggingIn(true);
-    // Simulate login process
-    setTimeout(() => {
-      // Redirect to the root URL after a 5-second delay
-      navigate('/');
-    }, 5000);
-  };
-
+const google_oauth_url = import.meta.env.VITE_GOOGLE_OAUTH_URL;
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
-        <div className="right w-[60%] flex min-h-[90vh]">
-          <Card className="self-center mx-auto">
-            <CardHeader>
-              <CardTitle className='mb-3'>Log In to Jumpstart Your Career</CardTitle>
-              <CardDescription className='mb-5'>
-                Securely log in now to start getting personalized resume feedback and discover exciting job opportunities tailored just for you
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Button onClick={handleLogin} disabled={isLoggingIn}>
-                      {isLoggingIn ? 'Logging in...' : 'Google'}
-                    </Button>
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-          </Card>
+          <div className="right w-[60%] flex min-h-[90vh]">
+            <Card className="self-center mx-auto">
+               <CardHeader>
+                   <CardTitle className='mb-3'>Log In to Jumpstart Your Career</CardTitle>
+                   <CardDescription className='mb-5'>Securely log in now to start getting personalized resume feedback and discover exciting job opportunities tailored just for you</CardDescription>
+               </CardHeader>
+              <CardContent>
+                   <form>
+                       <div className="grid w-full items-center gap-4">
+                           <div className="flex flex-col space-y-1.5">
+                           <Button asChild>
+                               <Link to={google_oauth_url}>Google</Link>
+                            </Button>
+                            </div>
+                        </div>
+                     </form>
+                </CardContent>
+             </Card>
         </div>
       </div>
       <div className="hidden bg-muted lg:block">
-        <img
-          src="/auth.jpg"
-          alt="auth"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-          width="1920"
-          height="1080"
-        />
+        <img src="/auth.jpg" alt="auth" className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" width="1920"
+          height="1080"/>
       </div>
     </div>
-  );
+  )
 }
 
-export default Auth;
+export default Auth
